b8623a522aba0e9b59b0c050587ffe6ad466b1db
Ad-AM <87221134+BCKS-AdAM@users.noreply.github.com>
2021-07-26 21:23:00 +0630

    Create blue.sh

---
+++ blue.sh
@@ -0,0 +1,107 @@
+
+termux-open fb://profile/100030025394608
+clear
+echo -e "\e[1;31m
+
+⣿⢸⣿⣿⣿⣿⣿⢹⣿⣿⣿⣿⣿⢿⣿⡇⡇⣿⣿⡇⢹⣿⣿⣿⣿⣿⣿⠄⢸⣿
+⡟⢸⣿⣿⣭⣭⡭⣼⣶⣿⣿⣿⣿⢸⣧⣇⠇⢸⣿⣿⠈⣿⣿⣿⣿⣿⣿⡆⠘⣿
+⡇⢸⣿⣿⣿⣿⡇⣻⡿⣿⣿⡟⣿⢸⣿⣿⠇⡆⣝⠿⡌⣸⣿⣿⣿⣿⣿⡇⠄⣿
+⢣⢾⣾⣷⣾⣽⣻⣿⣇⣿⣿⣧⣿⢸⣿⣿⡆⢸⣹⣿⣆⢥⢛⡿⣿⣿⣿⡇⠄⣿
+⣛⡓⣉⠉⠙⠻⢿⣿⣿⣟⣻⠿⣹⡏⣿⣿⣧⢸⣧⣿⣿⣨⡟⣿⣿⣿⣿⡇⠄⣿
+⠸⣷⣹⣿⠄⠄⠄⠄⠘⢿⣿⣿⣯⣳⣿⣭⣽⢼⣿⣜⣿⣇⣷⡹⣿⣿⣿⠁⢰⣿
+⠄⢻⣷⣿⡄⢈⠿⠇⢸⣿⣿⣿⣿⣿⣿⣟⠛⠲⢯⣿⣒⡾⣼⣷⡹⣿⣿⠄⣼⣿
+⡄⢸⣿⣿⣷⣬⣽⣯⣾⣿⣿⣿⣿⣿⣿⣿⣿⡀⠄⢀⠉⠙⠛⠛⠳⠽⠿⢠⣿⣿
+⡇⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⢄⣹⡿⠃⠄⠄⣰⠎⡈⣾⣿⣿
+⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣭⣽⣖⣄⣴⣯⣾⢷⣿⣿⣿
+⣧⠸⣿⣿⣿⣿⣿⣿⠯⠊⠙⢻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣾⣿⣿⣿
+⣿⣦⠹⣿⣿⣿⣿⣿⠄⢀⣴⢾⣼⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣾⣿⣿⣿⣿
+⣿⣿⣇⢽⣿⣿⣿⡏⣿⣿⣿⣿⣿⡇⣿⣿⣿⣿⡿⣿⣛⣻⠿⣟⣼⣿⣿⣿⣿⢃
+⣿⣿⣿⡎⣷⣽⠻⣇⣿⣿⣿⡿⣟⣵⣿⣟⣽⣾⣿⣿⣿⣿⢯⣾⣿⣿⣿⠟⠱⡟
+⣿⣿⣿⣿⢹⣿⣿⢮⣚⡛⠒⠛⢛⣋⣶⣿⣿⣿⣿⣿⣟⣱⠿⣿⣿⠟⣡⣺⢿
+
+\e[0m"
+
+echo ""
+
+echo -e "\e[1;32m        Code By Ad A M \e[0m"
+
+echo ""
+
+echo -e "\e[1;32m      !Only for boys over 18 years old! \e[0m"
+
+echo ""
+
+echo " 1. Home "
+echo""
+echo " 2. japan "
+echo ""
+echo " 3. Us "
+echo""
+echo " 4. UK "
+echo ""
+echo " 5. Hentai"
+echo""
+echo " 6. girl to girl "
+echo ""
+echo " 7. Boy to Boy "
+echo ""
+echo " 8. Myanmar "
+echo ""
+read -p " 😈Choose what you want to see : " ghost
+
+if [[ $ghost == 1 ]];then
+echo ""
+sleep 3 
+termux-open https://www.xnxx.com
+echo ""
+echo -e "\e[1;32m  I see You Bro"
+
+elif [[ $ghost == 2 ]];then
+echo ""
+sleep 3
+termux-open https://www.xnxx.com/search/Japan
+echo ""
+echo -e "\e[1;32m  I see You Bro "
+
+elif [[ $ghost == 3 ]];then
+echo ""
+sleep 3 
+termux-open https://www.xnxx.com/search/Us
+echo ""
+echo -e "\e[1;32m  I See You Bro  "
+
+elif [[ $ghost == 4 ]];then
+echo ""
+sleep 3 
+termux-open https://www.xnxx.com/search/Uk
+echo ""
+echo -e "\e[1;32m  I see You Bro"
+
+elif [[ $ghost == 5 ]];then
+echo""
+sleep 3
+termux-open https://www.xnxx.com/search/Hentai?top
+echo ""
+echo -e "\e[1;32m  I See You Bro "
+
+elif [[ $ghost == 6 ]];then
+echo ""
+sleep 3 
+termux-open https://www.xnxx.com/search/girl+to+girl
+echo ""
+echo -e "\e[1;32m  I See You Bro "
+elif [[ $ghost == 7 ]];then
+echo ""
+sleep 3 
+termux-open https://www.xnxx.com/search/boy+to+Boy
+echo ""
+echo -e "\e[1;32m   I see You Bro "
+elif [[ $ghost == 8 ]];then
+echo ""
+sleep 3 
+termux-open https://www.xnxx.com/search/Myanmar
+echo ""
+echo -e "\e[1;32m    I see You Bro"
+else
+echo -e "\e[1;33m Your country is not in list\e[0m"
+fi
